fe90d2824657e2bb85173da1d25da5e9943dfc46
Ray <rayforvideos@gmail.com>
2026-09-21 00:29:29 +0900

    chore: steiger insignificant-slice 예외 제거

--- steiger.config.ts
+++ steiger.config.ts
@@ -4,12 +4,4 @@ import { defineConfig } from "steiger";
 export default defineConfig([
   ...fsd.configs.recommended,
   { files: ["./src/shared/**"], rules: { "fsd/public-api": "off" } },
-  {
-    files: [
-      "./src/features/edit-content/**",
-      "./src/features/edit-notification/**",
-      "./src/features/publish-content/**",
-    ],
-    rules: { "fsd/insignificant-slice": "off" },
-  },
 ]);
