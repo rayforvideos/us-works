cf2850e4985d8e3425185372b3b38ce9a637c21b
Ray <rayforvideos@gmail.com>
2026-09-20 18:09:54 +0900

    feat: 콘텐츠 목록 페이지 구성

--- steiger.config.ts
+++ steiger.config.ts
@@ -4,5 +4,14 @@ import { defineConfig } from "steiger";
 export default defineConfig([
   ...fsd.configs.recommended,
   { files: ["./src/shared/**"], rules: { "fsd/public-api": "off" } },
-  { files: ["./src/widgets/gnb/**"], rules: { "fsd/insignificant-slice": "off" } },
+  {
+    files: [
+      "./src/widgets/gnb/**",
+      "./src/widgets/list-header/**",
+      "./src/widgets/content-table/**",
+      "./src/features/filter-contents/**",
+      "./src/entities/content/**",
+    ],
+    rules: { "fsd/insignificant-slice": "off" },
+  },
 ]);
